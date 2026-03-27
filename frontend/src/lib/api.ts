8814b544697f624864b8d122515d935277f7f2aa
Maxime Lucquin <m.lucquin@namkin.fr>
2026-03-27 09:01:34 +0100

    Dockerisation du projet

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,14 +1,14 @@
-import axios from 'axios'
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL ?? 'http://localhost:3000',
-})
+  baseURL: import.meta.env.VITE_API_URL ?? "http://localhost:8080",
+});
 
 // Injecte automatiquement le token JWT sur chaque requête
 api.interceptors.request.use((config) => {
-    const token = localStorage.getItem('token')
-    if (token) config.headers.Authorization = `Bearer ${token}`
-    return config
-})
+  const token = localStorage.getItem("token");
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
 
-export default api
+export default api;
