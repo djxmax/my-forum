eb303b16d3d7379067dd07efc1f920393eb91165
Maxime Lucquin <m.lucquin@namkin.fr>
2026-03-30 15:06:44 +0200

    Ajoout de la pagination sur les comment coté back

--- backend/apps/backend/src/controllers/comment.controller.ts
+++ backend/apps/backend/src/controllers/comment.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Get, Post, Delete, Param, Body, UseGuards } from '@nestjs/common'
+import { Controller, Get, Post, Delete, Param, Body, UseGuards, Query } from '@nestjs/common'
 import { CommentService } from '../services/comment.service'
 import { CreateCommentDto } from '../dto/comment.dto'
 import { JwtAuthGuard } from '@app/auth/jwt/jwt-auth.guard'
 import { CurrentUser } from '@app/auth'
 import { UserDocument } from '@app/models/users/user.schema'
 import { OptionalJwtAuthGuard } from '@app/auth/jwt/optional-jwt-auth-guard'
+import { PaginationDto } from '../dto/pagination.dto'
 
 @Controller('comments')
 export class CommentController {
@@ -12,8 +13,8 @@ export class CommentController {
 
     @Get('post/:postId')
     @UseGuards(OptionalJwtAuthGuard)
-    findByPost(@Param('postId') postId: string, @CurrentUser() user: UserDocument) {
-        return this.commentService.findByPost(postId, user)
+    findByPost(@Param('postId') postId: string, @CurrentUser() user: UserDocument, @Query() pagination: PaginationDto) {
+        return this.commentService.findByPost(postId, user, pagination)
     }
 
     @Post()
